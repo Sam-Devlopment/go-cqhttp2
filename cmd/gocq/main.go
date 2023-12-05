90eb25a99ae7d392fdafac51155e6bb0e4a83bd7
帝君 <32099406+MTmin@users.noreply.github.com>
2023-12-05 15:40:46 +0800

    Update main.go
    2

--- cmd/gocq/main.go
+++ cmd/gocq/main.go
@@ -390,6 +390,9 @@ func LoginInteract() {
 	cli.AllowSlider = true
 	log.Infof("登录成功 欢迎使用: %v", cli.Nickname)
 	log.Info("开始加载好友列表...")
+	err := cli.ReloadFriendList()
+	global.Check(err, true)
+	
 	log.Infof("共加载 %v 个好友.", len(cli.FriendList))
 	log.Infof("开始加载群列表...")
 	global.Check(cli.ReloadGroupList(), true)
